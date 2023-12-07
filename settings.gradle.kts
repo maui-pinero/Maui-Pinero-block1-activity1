970dff841dcc7f7785b4d2d87ce07dd6542490dc
Maui Pinero <mauipinero2003@gmail.com>
2023-12-07 17:28:21 +0800

    quiz

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MauiPinero-Block1-Quiz"
+include(":app")
